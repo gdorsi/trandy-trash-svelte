3cd5a6446bcc9a8e4c0c49188758775eab20de69
Guido D'Orsi <guido.dorsi@immobiliare.it>
2021-11-09 09:24:08 +0100

    feat: static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
+import static_adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: static_adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
