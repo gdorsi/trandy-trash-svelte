7b88044bfd2694a5633bcb9cc752378fa6e5bab0
Guido D'Orsi <guido.dorsi@immobiliare.it>
2021-11-09 09:31:48 +0100

    fix: base path for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import static_adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: static_adapter(),
+		paths: {
+			base: '/trandy-trash-svelte'
+		},
+		adapter: static_adapter({
+			pages: 'docs',
+			assets: 'docs',
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
