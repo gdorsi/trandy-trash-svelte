99761bacb7a9827a65c54535719d55d62739c2ac
Guido D'Orsi <guido.dorsi@immobiliare.it>
2021-11-09 09:43:31 +0100

    fix: toggle flag for github build

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,19 @@ import static_adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		paths: {
-			base: '/trandy-trash-svelte'
-		},
-		adapter: static_adapter({
-			pages: 'docs',
-			assets: 'docs',
-		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
 };
+
+if (process.env.GITHUB_PAGES) {
+	config.kit.paths = {
+		base: '/trandy-trash-svelte'
+	};
+	config.adapter = static_adapter({
+		pages: 'docs',
+		assets: 'docs',
+	});
+}
 
 export default config;
